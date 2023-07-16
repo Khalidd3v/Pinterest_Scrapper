cbd0fe898f51b66ee0c954715dbc7b4d3a1f414a
khalid ali khan <khalidbinalikhan@gmail.com>
2023-07-16 18:29:12 +0500

    now short links content can be downloaded too

--- app/views.py
+++ app/views.py
@@ -1,16 +1,22 @@
-
 import os
 import requests
 import re
 
 from django.shortcuts import render
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def home(request):
     if request.method == 'POST':
         video_link = request.POST.get('video_link')
         save_path = "/Users/khalid/Desktop/video.mp4"
-        
-        video_src = extract_video_src(video_link)
+
+        final_url, html_code = retrieve_html_code(video_link)
+
+        video_src = extract_video_src(html_code)
         if video_src:
             download_video(video_src, save_path)
         else:
@@ -18,22 +24,36 @@ def home(request):
 
     return render(request, 'app/home.html')
 
-def extract_video_src(video_link):
-    """Extracts the video source URL from the given URL."""
-    html_code = retrieve_html_code(video_link)
+def retrieve_html_code(url):
+    """Retrieves the HTML code from the given URL and follows redirects."""
+    options = Options()
+    options.add_argument("--headless")  # Run in headless mode
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    
+    # Wait for the page to load completely
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    
+    final_url = driver.current_url
+    html_code = driver.page_source
+    driver.quit()
+    
+    return final_url, html_code
+
+def extract_video_src(html_code):
+    """Extracts the video source URL from the given HTML code."""
     video_src = None
     pattern = r'url":"(https?://[^"]+\.mp4)"'
     match = re.search(pattern, html_code)
     if match:
         video_src = match.group(1)
-    
+
     if video_src:
         print(f"Scraped video source link: {video_src}")
     else:
         print("No valid video source link found.")
-        
-    return video_src
 
+    return video_src
 
 def download_video(video_src, save_path):
     """Downloads the video from the given source URL to the specified path."""
@@ -49,9 +69,3 @@ def download_video(video_src, save_path):
             print(f"Failed to download video from {video_src}")
     else:
         print("No valid video source URL to download.")
-
-def retrieve_html_code(url):
-    """Retrieves the HTML code from the given URL."""
-    response = requests.get(url)
-    html_code = response.text
-    return html_code
