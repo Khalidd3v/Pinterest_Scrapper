c4c9bb393e04af7480d9eab97fb90e69e48c8cb9
khalid ali khan <khalidbinalikhan@gmail.com>
2023-07-18 18:23:11 +0500

    changed into request library

--- app/views.py
+++ app/views.py
@@ -1,47 +1,156 @@
+# import os
+# import requests
+# import re
+
+# from django.shortcuts import render
+
+# def home(request):
+#     if request.method == 'POST':
+#         video_link = request.POST.get('video_link')
+#         save_path = "/Users/khalid/Desktop/video.mp4"
+
+#         video_src = process_link(video_link, save_path)
+#         if video_src:
+#             download_video(video_src, save_path)
+#         else:
+#             print("Failed to extract video source URL.")
+
+#     return render(request, 'app/home.html')
+
+# def process_link(video_link, save_path):
+#     """Processes the video link, handles redirection if necessary, and extracts the video source URL."""
+#     redirected_link = follow_redirection(video_link)
+#     if redirected_link:
+#         redirected_link2 = follow_redirection(redirected_link)
+#         if redirected_link2:
+#             video_src = extract_video_src(redirected_link2)
+#         else:
+#             video_src = extract_video_src(redirected_link)
+#     else:
+#         video_src = extract_video_src(video_link)
+
+#     return video_src
+
+# def follow_redirection(url):
+#     """Follows the redirection for the given URL and returns the final URL."""
+#     session = requests.Session()
+#     response = session.head(url, allow_redirects=True)
+#     final_url = response.url
+#     if final_url != url:
+#         print(f"Redirected to: {final_url}")
+#         return final_url
+
+#     return None
+
+# def extract_video_src(video_link):
+#     """Extracts the video source URL from the given URL."""
+#     html_code = retrieve_html_code(video_link)
+#     video_src = None
+#     pattern = r'url":"(https?://[^"]+\.mp4)"'
+#     match = re.search(pattern, html_code)
+#     if match:
+#         video_src = match.group(1)
+
+#     if video_src:
+#         print(f"Scraped video source link: {video_src}")
+#     else:
+#         print("No valid video source link found.")
+
+#     return video_src
+
+# def retrieve_html_code(url):
+#     """Retrieves the HTML code from the given URL."""
+#     response = requests.get(url)
+#     html_code = response.text
+#     return html_code
+
+# def download_video(video_src, save_path):
+#     """Downloads the video from the given source URL to the specified path."""
+#     if video_src:
+#         response = requests.get(video_src, stream=True)
+#         if response.status_code == 200:
+#             with open(save_path, "wb") as f:
+#                 for chunk in response.iter_content(chunk_size=1024):
+#                     if chunk:
+#                         f.write(chunk)
+#             print(f"Successfully downloaded video to {save_path}")
+#         else:
+#             print(f"Failed to download video from {video_src}")
+#     else:
+#         print("No valid video source URL to download.")
+
 import os
 import requests
 import re
-
+from bs4 import BeautifulSoup
 from django.shortcuts import render
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 def home(request):
     if request.method == 'POST':
+        # Get the video link from the form
         video_link = request.POST.get('video_link')
-        save_path = "/Users/khalid/Desktop/video.mp4"
 
-        final_url, html_code = retrieve_html_code(video_link)
+        # Get the response from the URL, following redirections
+        response = requests.get(video_link, allow_redirects=True)
 
-        video_src = extract_video_src(html_code)
-        if video_src:
-            download_video(video_src, save_path)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Get the HTML content of the page
+            html_content = response.text
+
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(html_content, 'html.parser')
+
+            # Find the canonical link in the webpage code
+            canonical_link = soup.find('link', {'rel': 'canonical'})
+
+            if canonical_link:
+                href = canonical_link.get('href')
+                print(f"Canonical link found: {href}")
+
+                # Call the rest of the functions with the canonical link
+                save_path = "/Users/khalid/Desktop/video.mp4"
+                video_src = process_link(href, save_path)
+                if video_src:
+                    download_video(video_src, save_path)
+                else:
+                    print("Failed to extract video source URL from the canonical link.")
+            else:
+                print("No canonical link found in the webpage.")
         else:
-            print("Failed to extract video source URL.")
+            print(f"Failed to download the webpage. Status code: {response.status_code}")
 
     return render(request, 'app/home.html')
 
-def retrieve_html_code(url):
-    """Retrieves the HTML code from the given URL and follows redirects."""
-    options = Options()
-    options.add_argument("--headless")  # Run in headless mode
-    driver = webdriver.Chrome(options=options)
-    driver.get(url)
-    
-    # Wait for the page to load completely
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-    
-    final_url = driver.current_url
-    html_code = driver.page_source
-    driver.quit()
-    
-    return final_url, html_code
 
-def extract_video_src(html_code):
-    """Extracts the video source URL from the given HTML code."""
+def process_link(video_link, save_path):
+    """Processes the video link, handles redirection if necessary, and extracts the video source URL."""
+    redirected_link = follow_redirection(video_link)
+    if redirected_link:
+        redirected_link2 = follow_redirection(redirected_link)
+        if redirected_link2:
+            video_src = extract_video_src(redirected_link2)
+        else:
+            video_src = extract_video_src(redirected_link)
+    else:
+        video_src = extract_video_src(video_link)
+
+    return video_src
+
+def follow_redirection(url):
+    """Follows the redirection for the given URL and returns the final URL."""
+    session = requests.Session()
+    response = session.head(url, allow_redirects=True)
+    final_url = response.url
+    if final_url != url:
+        print(f"Redirected to: {final_url}")
+        return final_url
+
+    return None
+
+def extract_video_src(video_link):
+    """Extracts the video source URL from the given URL."""
+    html_code = retrieve_html_code(video_link)
     video_src = None
     pattern = r'url":"(https?://[^"]+\.mp4)"'
     match = re.search(pattern, html_code)
@@ -54,6 +163,12 @@ def extract_video_src(html_code):
         print("No valid video source link found.")
 
     return video_src
+
+def retrieve_html_code(url):
+    """Retrieves the HTML code from the given URL."""
+    response = requests.get(url)
+    html_code = response.text
+    return html_code
 
 def download_video(video_src, save_path):
     """Downloads the video from the given source URL to the specified path."""
@@ -69,3 +184,4 @@ def download_video(video_src, save_path):
             print(f"Failed to download video from {video_src}")
     else:
         print("No valid video source URL to download.")
+
